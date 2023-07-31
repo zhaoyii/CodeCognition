045773de916e5e9933d21605972ef6a1372f0d4f
andrew.zhao <andrew.zhao@wayz.ai>
2023-07-31 11:18:18 +0800

    news:add tokenizers

---
+++ 20230729_transformers/to_tokenizers/to_tokenizers.py
@@ -0,0 +1,20 @@
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+
+tokenizer = Tokenizer(BPE())
+
+from tokenizers.pre_tokenizers import Whitespace
+
+tokenizer.pre_tokenizer = Whitespace()
+
+from tokenizers.trainers import BpeTrainer
+
+trainer = BpeTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"])
+tokenizer.train(files=["wiki.train.raw", "wiki.valid.raw", "wiki.test.raw"], trainer=trainer)
+
+output = tokenizer.encode("Hello, y'all! How are you 😁 ?")
+print(output.tokens)
+# ["Hello", ",", "y", "'", "all", "!", "How", "are", "you", "[UNK]", "?"]
+
+output = tokenizer.encode("We are very happy to show you the 🤗 Transformers library.")
+print(output.tokens)
